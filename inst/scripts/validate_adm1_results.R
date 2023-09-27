b75865c9f7f02aeca7b9501f2f504249da62c763
Nathaniel Henry <nat.henry7@gmail.com>
2023-09-26 17:50:08 -0700

    Save fuzzy-matched results to file.

--- inst/scripts/validate_adm1_results.R
+++ inst/scripts/validate_adm1_results.R
@@ -116,6 +116,16 @@ summary_text <- glue::glue(
 )
 message(summary_text)
 
+# Save the raw StatCompiler data and merged estimates
+versioning::autowrite(
+  sc_summaries,
+  file = file.path(viz_dir, 'statcompiler_estimates.csv')
+)
+versioning::autowrite(
+  fuzzy_matches,
+  file = file.path(viz_dir, 'statcompiler_estimates_matched.csv')
+)
+
 
 ## 04) Plot admin1 comparison ----------------------------------------------------------->
 
@@ -143,165 +153,4 @@ fig <- ggplot(data = fuzzy_matches, aes(x = sc_value, y = mean, color = in_ui)) 
 
 pdf(file.path(viz_dir, "statcompiler_vetting.pdf"), height = 8, width = 8)
 print(fig)
-dev.off()
-
-
-
-
-# See the config.yaml file for the list of all results files that can be loaded
-modeling_level <- config$get('shapefile_settings', 'modeling_level')
-adm_boundaries <- config$read('results', 'adm_boundaries')
-summary_table <- config$read('results', glue::glue('adm{modeling_level}_summary_table'))
-mean_raster <- config$read('results', 'cell_pred_mean')
-lower_raster <- config$read('results', 'cell_pred_lower')
-upper_raster <- config$read('results', 'cell_pred_upper')
-mesh <- config$read('results', 'inla_data_stack')$mesh
-
-# Set some global variables that will be used in labels several times
-indicator <- config$get('indicator')
-country <- config$get('country')
-year <- config$get('year')
-
-# Set the color scheme for mean/lower/upper values
-low_is_better <- TRUE
-color_scheme <- RColorBrewer::brewer.pal(n = 9, name = 'Spectral')
-if(low_is_better) color_scheme <- rev(color_scheme)
-
-
-## 02) Plot the mesh -------------------------------------------------------------------->
-
-pdf(file.path(viz_dir, 'inla_mesh.pdf'), height = 10, width = 10)
-plot(mesh)
-dev.off()
-
-
-## 03) Plot the summary rasters --------------------------------------------------------->
-
-raster_to_table <- function(rr, type){
-  r_table <- as.data.frame(rr, xy = T)
-  names(r_table) <- c('x','y','value')
-  r_table$type = type
-  return(r_table)
-}
-raster_table_full <- rbind(
-  raster_to_table(mean_raster, type = '1. Mean'),
-  raster_to_table(lower_raster, type = '2. Lower'),
-  raster_to_table(upper_raster, type = '3. Upper')
-)
-value_limits <- quantile(na.omit(raster_table_full$value), probs = c(0.05, 0.95))
-
-raster_fig <- ggplot() + 
-  facet_wrap('type', nrow = 1) +
-  geom_raster(data = raster_table_full, aes(x = x, y = y, fill = value)) + 
-  geom_sf(data = adm_boundaries, fill = NA, linewidth = 0.05, color = '#444444') + 
-  scale_fill_gradientn(
-    colors = color_scheme, limits = value_limits, oob = scales::squish,
-    labels = scales::percent
-  ) +
-  labs(
-    title = glue::glue("Summary rasters: {indicator} in {country}, {year}"),
-    x = '', y = '', fill = indicator
-  ) +
-  theme_minimal() + 
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid = element_blank()
-  )
-pdf(file.path(viz_dir, 'summary_rasters.pdf'), height = 6, width = 10)
-print(raster_fig)
-dev.off()
-
-
-## 03) Plot the summary administrative data --------------------------------------------->
-
-adm_ids <- config$get('shapefile_settings', 'ids', paste0('adm', modeling_level))
-adm_data_for_plotting <- merge(
-  x = adm_boundaries,
-  y = melt(summary_table, id.vars = adm_ids),
-  by = adm_ids
-) |> merge(
-  y = data.frame(
-    variable = c('mean','lower','upper'),
-    type = c('1. Mean', '2. Lower', '3. Upper')
-  ),
-  by = 'variable'
-)
-
-adm_fig <- ggplot() + 
-  facet_wrap('type', nrow = 1) +
-  geom_sf(
-    data = adm_data_for_plotting,
-    aes(fill = value), linewidth = 0.25, color = '#444444'
-  ) + 
-  scale_fill_gradientn(
-    colors = color_scheme, limits = value_limits, oob = scales::squish,
-    labels = scales::percent
-  ) +
-  labs(
-    title = glue::glue("Summary estimates: {indicator} in {country}, {year}"),
-    x = '', y = '', fill = indicator
-  ) +
-  theme_minimal() + 
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid = element_blank()
-  )
-pdf(file.path(viz_dir, 'summary_admin_estimates.pdf'), height = 6, width = 10)
-print(adm_fig)
-dev.off()
-
-
-## 04) Plot mean vs. uncertainty bivariate maps ----------------------------------------->
-
-bv_color_df <- data.table::CJ(uncertainty_group = 0:2, value_group = 0:2)
-bv_color_df[, idx := as.character(.I) ]
-bv_color_vec <- c(
-  "#e8e8e8", "#e4acac", "#c85a5a", "#b0d5df", "#ad9ea5", "#985356",
-  "#64acbe", "#627f8c", "#574249"
-)
-names(bv_color_vec) <- bv_color_df$idx
-color_scheme_fig <- ggplot() +
-  geom_raster(data = bv_color_df, aes(x = value_group, y = uncertainty_group, fill = idx)) + 
-  labs(title = '', x = 'Mean estimates', y = 'Uncertainty') +
-  scale_fill_manual(values = bv_color_vec) +
-  guides(fill = 'none') +
-  theme_minimal() + 
-  theme(
-    axis.text = element_blank(), panel.grid = element_blank(), axis.ticks = element_blank(),
-    axis.title = element_text(size = 12, face = 'italic'),
-    plot.background = element_rect(fill='transparent', color=NA)
-  )
-
-bv_keep_fields <- c(adm_ids, 'idx')
-bv_data_dt <- (copy(summary_table)
-  [, ui := upper - lower ]
-  [, value_group := 2 ]
-  [ mean < quantile(mean, 2/3, na.rm= T), value_group := 1]
-  [ mean < quantile(mean, 1/3, na.rm= T), value_group := 0 ]
-  [, uncertainty_group := 2 ]
-  [ ui < quantile(ui, 2/3, na.rm= T), uncertainty_group := 1 ]
-  [ ui < quantile(ui, 1/3, na.rm= T), uncertainty_group := 0 ]
-  [bv_color_df, idx := i.idx, on = c('value_group', 'uncertainty_group')]
-  [, ..bv_keep_fields]
-)
-bv_plot_data <- merge(
-  x = adm_boundaries,
-  y = bv_data_dt,
-  by = adm_ids
-)
-
-bv_fig <- ggplot() + 
-  geom_sf(data = bv_plot_data, aes(fill = idx), linewidth = 0.25, color = '#444444') + 
-  scale_fill_manual(values = bv_color_vec) +
-  guides(fill = 'none') +
-  theme_void()
-
-full_fig <- bv_fig +
-  (patchwork::plot_spacer() / color_scheme_fig / patchwork::plot_spacer()) +
-  patchwork::plot_layout(nrow = 1, widths = c(6, 2))
-
-pdf(file.path(viz_dir, 'bivariate_uncertainty_plot.pdf'), height = 8, width = 8)
-plot(full_fig)
 dev.off()
