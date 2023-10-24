f839f94dc593ca8d7118c2aa7d4dfe010f9f80c5
Nathaniel Henry <nat.henry7@gmail.com>
2023-10-23 19:16:26 -0700

    Move covariate specifications to a covariate_table CSV file within the raw_data folder.

--- R/load_covariates.R
+++ R/load_covariates.R
@@ -8,9 +8,8 @@ get_current_year <- function(){
 #' @details Load a list of covariates from a specified directory structure
 #' 
 #' @param directory Directory containing all covariate sub-directories
-#' @param settings Nested named list, where names of the top-level list correspond to the
-#'   names of each covariate. Each sub-list corresponds to the settings for that
-#'   covariate. Settings include:
+#' @param covariates_table `data.frame` containing at least the following fields:
+#'   - 'covariate': (character): Name of the covariate
 #'   - 'annual': (logical) Does the covariate vary by year? If so, look for the `year` in
 #'     the name of the file.
 #'   - 'transform': (character) Name of a function to use to transform the covariate. 
@@ -22,8 +21,11 @@ get_current_year <- function(){
 #'   study area 
 #' @param year (numeric, default NULL) Year of data to for time-varying covariates.
 #'   If NULL, the default, uses the current year.
-#' @param file_format (character, default 'tif')
-#' @param add_intercept (logical, default FALSE)
+#' @param file_format (character, default 'tif') File format for the raster covariate
+#'   data. Used to search for the input file within the proper containing folder.
+#' @param add_intercept (logical, default FALSE) Should a covariate called "intercept",
+#'   a raster object with 1s in all required cells, be placed at the start of the returned
+#'   covariates list?
 #' 
 #' @return A named list of formatted covariates. Each list item is a terra SpatRaster with
 #'   one layer and the same dimensions as the `id_raster`
@@ -32,14 +34,17 @@ get_current_year <- function(){
 #' @importFrom terra rast crop mask global
 #' @export
 load_covariates <- function(
-  directory, settings, id_raster, year = NULL, file_format = 'tif', add_intercept = FALSE
+  directory, covariates_table, id_raster, year = NULL, file_format = 'tif',
+  add_intercept = FALSE
 ){
   # Input data checks
   assertthat::assert_that(dir.exists(directory))
-  assertthat::assert_that(inherits(settings, 'list'))
-  assertthat::assert_that(!is.null(names(settings)), msg = 'settings must be a named list.')
+  assertthat::assert_that(inherits(covariates_table, 'data.frame'))
+  assertthat::assert_that(
+    all(c('covariate', 'annual', 'transform', 'normalize') %in% colnames(covariates_table))
+  )
   assertthat::assert_that(inherits(id_raster, 'SpatRaster'))
-  if(is.null(year)) year <- get_current_year
+  if(is.null(year)) year <- get_current_year()
   year <- as.integer(year)
   assertthat::assert_that(length(year) == 1)
   assertthat::assert_that(!is.na(year))
@@ -49,21 +54,15 @@ load_covariates <- function(
   assertthat::assert_that(length(add_intercept) == 1)
 
   # Load each covariate based on its settings
-  cov_names <- names(settings)
+  cov_names <- covariates_table$covariate
   covariates_list <- vector('list', length = length(cov_names))
   names(covariates_list) <- cov_names
 
-  for(cov_name in names(settings)){
-    cov_settings <- settings[[cov_name]]
+  for(cov_name in cov_names){
+    cov_settings <- as.list(covariates_table[covariates_table$covariate == cov_name, ])
     # Check settings for this covariate
-    assertthat::assert_that(
-      all(c('annual', 'transform','normalize') %in% names(cov_settings))
-    )
-    assertthat::assert_that(length(cov_settings$annual) == 1)
     assertthat::assert_that(inherits(cov_settings$annual, 'logical'))
-    assertthat::assert_that(length(cov_settings$transform) == 1)
     assertthat::assert_that(inherits(cov_settings$transform, 'character'))
-    assertthat::assert_that(length(cov_settings$normalize) == 1)
     assertthat::assert_that(inherits(cov_settings$normalize, 'logical'))
     # The covariate sub-directory is based on the covariate name
     cov_sub_dir <- file.path(directory, cov_name)
